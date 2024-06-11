e8fdc62457db355667977127dfb86fe891f11b46
Toby-Fm <mail@toby-wichmann.de>
2024-06-11 23:11:05 +0200

    Window Task durchgehen, auslesen und die namen ausgeben.

--- src/include/readTask.hpp
+++ src/include/readTask.hpp
@@ -16,11 +16,26 @@ void readTask() {
     // Schnappschuss der aktuellen Prozesse erstellen
     hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (hProcessSnap == INVALID_HANDLE_VALUE) {
-        std::cout << "CreateToolhelp32Snapshot (of processes)" << std::endl;
+        std::cout << "Fehler beim erstellen vom Snapshot" << std::endl;
         return;
     }
 
+    pe32.dwSize = sizeof(PROCESSENTRY32);
 
+    // Den ersten Prozess im Schnappschuss abrufen
+    if (!Process32First(hProcessSnap, &pe32)) {
+        std::cout << "Fehler den ersten Prozess zu bekommen" << std::endl;
+        CloseHandle(hProcessSnap);
+        return;
+    }
+
+    // Alle Prozesse im Schnappschuss durchgehen und ihre Namen ausgeben
+    do {
+        std::wcout << "Prozessname: " << pe32.szExeFile << std::endl;
+    } while (Process32Next(hProcessSnap, &pe32));
+
+    // Schnappschuss-Handle schließen
+    CloseHandle(hProcessSnap);
 }
 
 #endif //READTASK_HPP
